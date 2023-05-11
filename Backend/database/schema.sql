0da76a6cd62385db73d982dd5235cb161e7e07d7
^[[200~Islem-Az <isleem.azouuz@gmail.com>
2023-05-11 11:45:09 +0200

    some changes on the post details , style of all postts

--- Backend/database/schema.sql
+++ Backend/database/schema.sql
@@ -15,33 +15,15 @@ CREATE SCHEMA IF NOT EXISTS `pictago` DEFAULT CHARACTER SET utf8 ;
 USE `pictago` ;
 
 -- -----------------------------------------------------
--- Table `pictago`.`user`
+-- Table `pictago`.`posts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pictago`.`user` (
-  `ID_user` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`ID_user`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `pictago`.`post`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pictago`.`post` (
+CREATE TABLE IF NOT EXISTS `pictago`.`posts` (
   `ID_post` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
-  `url_image` LONGTEXT NOT NULL,
-  `user_ID_user` INT NOT NULL,
-  PRIMARY KEY (`ID_post`, `user_ID_user`),
-  INDEX `fk_post_user_idx` (`user_ID_user` ASC) VISIBLE,
-  CONSTRAINT `fk_post_user`
-    FOREIGN KEY (`user_ID_user`)
-    REFERENCES `pictago`.`user` (`ID_user`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `image_url` LONGTEXT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  PRIMARY KEY (`ID_post`))
 ENGINE = InnoDB;
 
 
@@ -50,16 +32,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pictago`.`comments` (
   `ID_comment` INT NOT NULL AUTO_INCREMENT,
-  `body` VARCHAR(255) NOT NULL,
-  `post_ID_post` INT NOT NULL,
-  `post_user_ID_user` INT NOT NULL,
-  PRIMARY KEY (`ID_comment`, `post_ID_post`, `post_user_ID_user`),
-  INDEX `fk_comments_post1_idx` (`post_ID_post` ASC, `post_user_ID_user` ASC) VISIBLE,
-  CONSTRAINT `fk_comments_post1`
-    FOREIGN KEY (`post_ID_post` , `post_user_ID_user`)
-    REFERENCES `pictago`.`post` (`ID_post` , `user_ID_user`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `body` LONGTEXT NOT NULL,
+  PRIMARY KEY (`ID_comment`))
 ENGINE = InnoDB;
 
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,65 @@
-const connection=require("./database/index")
-const express=require('express')
-const cors=require('cors')
-const app=express()
-const route=require("./route")
-
+const express = require('express');
+const path = require('path');
+const cors = require("cors")
+const PORT = 5000;
+const app = express();
 app.use(cors())
 app.use(express.json())
+const connection= require('./database/index');
 
 
-app.use("/user",route)
+app.get('/pins',(req,res) => {
+ connection.query('SELECT * FROM posts',function(err, result) {
+    if (err) {
+      console.log(err)
+    }
+    res.send(result)
+  })
+})
 
 
 
-app.listen(5000,()=>console.log(`server is listening on 5000`))
+app.post("/addpost", (req,res) => {
+const {title,description,created_at,image_url} = req.body
+  connection.query("INSERT INTO posts SET ?",{title,description,created_at,image_url},(err) => {
+    if (err) return res.send(err)
+    res.send("post added")
+  });
+});
+
+
+
+
+
+
+app.delete("/deletePost/:ID_post" , (req , res ) => {
+  const {ID_post} = req.params
+connection.query("DELETE from posts WHERE ID_post = ?" , ID_post  , (err) => {
+  if (err) {  console.log(err);}
+else {res.send("post deleted ") }
+
+}) 
+})
+
+
+app.put('/updatePost/:ID_post', (req, res) => {
+  const { ID_post } = req.params;
+  const { title, description ,created_at,image_url } = req.body;
+  connection.query(
+    "UPDATE posts SET title = ?, `description` = ?, created_at = ?, image_url = ? WHERE ID_post = ?",
+    [title, description ,created_at,image_url,ID_post],
+    (error, result) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`post updated: ${title}`);
+        res.send('post updated');
+      }
+    }
+  );
+});
+
+
+
+
+app.listen(PORT,()=>console.log(`Server listening on port : ${PORT}`))
