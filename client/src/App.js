842c4f7d4729a43c89e7313b4d85895b1c9fc28c
^[[200~Islem-Az <isleem.azouuz@gmail.com>
2023-05-12 17:51:03 +0200

    Landpage , changed all the features and functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-
 import "./App.css";
+import PictagoLandingPage from "./components/landingPage/LandingPage";
 import AllPost from "./components/allPost/AllPost";
 import PostDetails from "./components/PostDetails/PostDetails";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
@@ -8,23 +8,22 @@ import AddPost from "./components/addPost/AddPost";
 import UpdatePost from "./components/update/updatePost";
 
 
+
 function App() {
   return (
     <div>
- 
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path="/add" element={<AddPost />}/>
-        <Route path="/" element={<AllPost />}/>
-        <Route path="/post/:ID_post" element={<PostDetails />} />
-        <Route path="/update/:ID_post" element={<UpdatePost/>} />
-  
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PictagoLandingPage />} />
+          <Route path="/add" element={<AddPost />}/>
+          <Route path="/posts" element={<AllPost />}/>
+          <Route path="/post/:ID_post" element={<PostDetails />} />
+          <Route path="/update/:ID_post" element={<UpdatePost/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
